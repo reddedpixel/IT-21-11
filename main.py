915efb7cd2f53697174461189336fe4aaa6709f9
reddedpixel <74682334+reddedpixel@users.noreply.github.com>
2020-11-19 23:49:58 +0300

    added helpful notes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+print ('Input angle A')
 a = int(input())
+print ('Input angle B')
 b = int(input())
+print ('Input angle C')
 c = int(input())
 
 if a+b+c == 180:
-    print ('Correct')
+    print ('Triangle exists.')
 else:
-    print ('Incorrect')
+    print ('Triangle does not exist.')
