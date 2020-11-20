dea62b41304d45b98c3b75d246b616ba80470f66
reddedpixel <74682334+reddedpixel@users.noreply.github.com>
2020-11-20 18:24:12 +0300

    fixed mistakes + pep8

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-
-#print ('Input Num A')
-#a = int(input())
-#print ('Input Num B')
-#b = int(input())
-#print ('Input Num C')
-#c = int(input())
-#
-#if a+b+c == 180:
-#    print ('Triangle exists.')
-#else:
-#    print ('Triangle does not exist.')
-
-
 class Num:
-    def __init__(self, a, b, c, x):
-        Num.x = x
-        Num.a = a
-        Num.b = b
-        Num.c = c
+    def __init__(self, x):
+        self.x = x
 
-    def check(self, x):
-        if Num.a+Num.b+Num.c == Num.x:
+    def check(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        if self.a+self.b+self.c == self.x:
             print('Correct')
-        else: print('Incorrect')
+        else:
+            print('Incorrect')
 
-print ('Input Num A')
+
+print('Input Num A')
 a = int(input())
-print ('Input Num B')
+print('Input Num B')
 b = int(input())
-print ('Input Num C')
+print('Input Num C')
 c = int(input())
 
 print('Input num X')
 x = int(input())
 
-f = Num(a, b, c, x)
-Num.check(f, x)
+f = Num(x)
+f.check(a, b, c)
