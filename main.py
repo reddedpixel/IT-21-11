02744b24a6d9e96f781e0555b4ec436a251c8ab2
reddedpixel <74682334+reddedpixel@users.noreply.github.com>
2020-12-20 01:10:46 +0300

    added v2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Element:
-    def __init__(self, key, value):
-        self.key = key
+
+    def __init__(self, value):
         self.value = value
+        self.next = None
 
 
 class Stack:
@@ -9,20 +10,27 @@ class Stack:
     def __init__(self):
         self.top = None
 
-    def push(self, key, value):
+    def push(self, value):
         if self.top is None:
-            self.top = Element(key, value)
-        elif self.top.key < key:
-            self.top = Element(key, value)
+            self.top = Element(value)
 
-    def printout(self):
-        print(self.top.key, self.top.value)
+        else:
+            newelement = Element(value)
+            newelement.next = self.top
+            self.top = newelement
+
+    def pop(self):
+        if self.top.next is None:
+            return None
+        else:
+            poppedelement = self.top
+            self.top = self.top.next
+            print(poppedelement.value)
 
 
 c1 = Stack()
-print('Input key')
-a = int(input())
-print('Input value')
-b = int(input())
-c1.push(a, b)
-c1.printout()
+c1.push(11)
+print(c1.top.value)
+c1.push(22)
+print(c1.top.value)
+c1.pop()
