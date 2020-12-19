4277fb58b22e39a1123aeb56e3647724cc3a359a
reddedpixel <74682334+reddedpixel@users.noreply.github.com>
2020-12-19 23:01:13 +0300

    added v1

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-print ('Input angle A')
-a = int(input())
-print ('Input angle B')
-b = int(input())
-print ('Input angle C')
-c = int(input())
+class Element:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
 
-if a+b+c == 180:
-    print ('Triangle exists.')
-else:
-    print ('Triangle does not exist.')
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+
+    def push(self, key, value):
+        if self.top is None:
+            self.top = Element(key, value)
+        elif self.top.key < key:
+            self.top = Element(key, value)
+
+    def printout(self):
+        print(self.top.key, self.top.value)
+
+
+c1 = Stack()
+print('Input key')
+a = int(input())
+print('Input value')
+b = int(input())
+c1.push(a, b)
+c1.printout()
